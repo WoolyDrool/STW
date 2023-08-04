7cadf61f3911efbeca1aa85efdf84804b32937fa
WoolyDrool <nikolai.mcneely15@gmail.com>
2023-08-03 20:18:59 -0700

    More non-changes

--- entities/player/CameraHolder.gd
+++ entities/player/CameraHolder.gd
@@ -5,10 +5,10 @@ var mouse_sensitivity := 0.1
 @onready var _cam := $Camera3D
 
 func _ready() -> void:
-    Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event: InputEvent) -> void:
-    if event is InputEventMouseMotion && Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-        _cam.rotate_x(deg_to_rad(-event.relative.x * mouse_sensitivity))
-        rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
-        _cam.rotation_degrees.x = clamp(_cam.rotation_degrees.x, -89.9, 89.9)
+	if event is InputEventMouseMotion && Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		_cam.rotate_x(deg_to_rad(-event.relative.x * mouse_sensitivity))
+		rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
+		_cam.rotation_degrees.x = clamp(_cam.rotation_degrees.x, -89.9, 89.9)
