00e7db83942781e779a9964a5c65c6076625bf19
WoolyDrool <nikolai.mcneely15@gmail.com>
2023-08-11 10:25:49 -0700

    Implemented boilerplate state logic from gdquest tutorial

--- systems/abstract_state.gd
+++ systems/abstract_state.gd
@@ -1,25 +1,29 @@
 extends Node
 
+# Stolen from GDQuest
+# https://www.gdquest.com/tutorial/godot/design-patterns/finite-state-machine/
+
 class_name AbstractState
 
 @export var StateName : String = "Default"
 var on : bool
+var machine = null
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+# Virtual functions - called by the AbsctractStateMachine 
 
-func Enter():
-	on = true
+func handle_input(_event : InputEvent) -> void:
+	pass
+	
+func update(_delta: float) -> void:
+	pass
+	
+func physics_update(_delta:float) -> void:
 	pass
 
-func _process(delta):
-	if on:
-		# do things
-		pass
+func enter(_msg := {}) -> void:
+	pass
 
-func Exit():
-	on = false
+func exit() -> void:
 	pass
 
 
