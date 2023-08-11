dff57f94041326127170dc8a014077b64a9a6397
WoolyDrool <nikolai.mcneely15@gmail.com>
2023-08-11 10:59:51 -0700

    Added two more possible signals the weenie will need

--- systems/ai/weenies/AIWeenie.gd
+++ systems/ai/weenies/AIWeenie.gd
@@ -10,7 +10,8 @@ signal make_self_known # Ping itself to Kour for whatever reason
 signal i_am_important # A higher priority version of make_self_known
 signal forget_me # Maybe there will be a need for a weenie to ask Kour to ignore it instead
 signal i_have_information # Maybe a node should only inform Kour of something, not request an action
-
+signal do_specific_thing # A specific request to a function or something
+signal stop_doing_this # A specific request to stop doing something
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
