5005fa632a8b5914e03ff7aba0a13b33ad5e8d53
WoolyDrool <nikolai.mcneely15@gmail.com>
2023-08-11 10:36:25 -0700

    Implemented base logic for the state machine, also using gdquest

--- systems/abstract_state_machine.gd
+++ systems/abstract_state_machine.gd
@@ -1,28 +1,38 @@
 extends Node
 
+# Also stolen from GDQuest 
+# https://www.gdquest.com/tutorial/godot/design-patterns/finite-state-machine/
+
 class_name AbstractStateMachine
+
+signal transitioned(state_name) # Good for them
+
+@export var initial_state := NodePath()
+@onready var state: AbstractState = get_node(initial_state)
 
 var currentState : AbstractState
 var nextState : AbstractState
 var previousState : AbstractState
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _ready() -> void:
+	await owner.ready
+	for child in get_children():
+		child.machine = self
+	state.enter()
 
+func _unhandled_input(event: InputEvent) -> void:
+	state.handle_input(event)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	state.update(delta)
 
-func ChangeState(state):
-	if state.StateName != currentState.StateName:
-		previousState = currentState
-		previousState.Exit()
-		currentState = state
-		currentState.Enter()
-		previousState = null
-
-func QueueState(state):
-	if state.StateName != previousState.StateName:
-		nextState = state
+func transition_to(target_state_name: String, msg: Dictionary = {}) -> void:
+	if not has_node(target_state_name):
+		return
+	
+	state.exit()
+	state = get_node(target_state_name)
+	state.enter(msg)
+	emit_signal("transitioned", state.name)
