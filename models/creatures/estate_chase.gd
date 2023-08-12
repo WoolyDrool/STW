ba84837ea5e933655247cb1111f2f1d0fa3b0caa
WoolyDrool <nikolai.mcneely15@gmail.com>
2023-08-11 20:50:05 -0700

    Began defining basic chase state

---
+++ models/creatures/estate_chase.gd
@@ -0,0 +1,19 @@
+extends EnemyState
+
+@export var movement_speed = 5
+
+func enter(_msg := {}) -> void:
+	print("Entered Chase State")
+
+func physics_update(delta: float) -> void:
+	if queen.nav_agent.is_navigation_finished():
+		return
+
+	var current_agent_position: Vector3 = queen.global_position
+	var next_path_position: Vector3 = queen.nav_agent.get_next_path_position()
+
+	var new_velocity: Vector3 = next_path_position - current_agent_position
+	new_velocity = new_velocity.normalized()
+	new_velocity = new_velocity * movement_speed
+
+	queen.velocity = new_velocity
